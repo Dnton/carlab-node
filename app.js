6372c7066615728dde23b8a2d6384e942200836e
MacBook Pro <MacBookPro@MacBook-Pros-MacBook-Pro.local>
2016-10-10 14:23:52 +0800

    test commit

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ var carType = require('./car')
 
 var yellowCar = new carType('yellow', 0)
 
-console.log('yellow car speed is ' + yellowCar.speed)
+console.log('yellow car speed is ' + yellowCar.speed
+)
+
+// this is only a test, I repeat this is only a test
