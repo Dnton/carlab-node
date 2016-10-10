988d6261ce74eb7a45d667e6f84eaccc63ce233b
MacBook Pro <MacBookPro@MacBook-Pros-MacBook-Pro.local>
2016-10-10 12:27:07 +0800

    second commit

---
+++ app.js
@@ -0,0 +1,5 @@
+var carType = require('./car')
+
+var yellowCar = new carType('yellow', 0)
+
+console.log('yellow car speed is ' + yellowCar.speed)
---
+++ car.js
@@ -0,0 +1,19 @@
+var Carspecs = function (color, speed, convertible) {
+  this.color = color;
+  this.speed = speed;
+  this.convertible = convertible;
+  this.accelerate = function(rate) {
+    return this.speed += rate
+  };
+  this.decelerate = function(rate) {
+    return this.speed -= rate
+  }
+}
+
+// var honda = new Carspecs('Honda', 'red', 0)
+//
+// console.log('I am a ' + honda.convertible + ', my current speed is ' + honda.speed +
+//             ', my accelerated speed is ' + honda.accelerate(100) +
+//             ', my decelerated speed is ' + honda.decelerate(20) )
+
+module.exports = Carspecs
